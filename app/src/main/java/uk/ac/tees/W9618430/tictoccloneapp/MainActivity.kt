69f5d91085fa19ec7c64bb84aa3f88c47d93d391
Nikhila Koppula <nikhilareddykoppula@gmail.com>
2023-12-04 05:05:32 +0000

    Second Commit

--- app/src/main/java/uk/ac/tees/W9618430/tictoccloneapp/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/W9618430/tictoccloneapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package uk.ac.tees.W9618430.tictoccloneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uk.ac.tees.W9618430.tictoccloneapp.databinding.ActivityMainBinding
+import uk.ac.tees.W9618430.tictoccloneapp.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavBar.setOnItemSelectedListener {menuItem->
+            when(menuItem.itemId){
+                R.id.bottom_menu_home->{
+                    UiUtil.showToast(this,"Home")
+                }
+                R.id.bottom_menu_add_video->{
+                    startActivity(Intent(this,VideoUploadActivity::class.java))
+                }
+                R.id.bottom_menu_profile->{
+                    UiUtil.showToast(this,"Profile")
+                    //Goto ProfileActivity
+                }
+            }
+            false
+
+
+        }
     }
 }
